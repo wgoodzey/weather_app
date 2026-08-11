5b3495c0841e4f4236fa21b945a08b0c3b9bf95e
Walter Goodzey <145727171+wgoodzey@users.noreply.github.com>
2026-08-10 22:57:41 -0500

    adding printing of weather url

--- src/weather.rs
+++ src/weather.rs
@@ -40,5 +40,8 @@ pub fn fetch(
     let url = format!(
         "https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=sunrise,sunset&hourly=temperature_2m,precipitation,weather_code&models=dwd_icon_seamless&current=weather_code,temperature_2m,apparent_temperature,precipitation,is_day&timezone=auto&forecast_days={days}&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch"
     );
+
+    println!("{url}");
+
     client.get(&url).send()?.json()
 }
